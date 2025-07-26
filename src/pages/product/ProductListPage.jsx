da688cb462df4b56baeb8a7e86002a82f58591c4
Jallen Portugal <95646179+Liebe3@users.noreply.github.com>
2025-07-26 09:22:07 +0800

    feature: featch data and display product list with filtering for missing images

--- src/pages/product/ProductListPage.jsx
+++ src/pages/product/ProductListPage.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import { useTime } from "motion/react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../../components/states/Loading";
+
+import ProductItem from "../../components/product/ProductItem";
 
 const ProductListPage = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const withoutImageId = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 28];
+
+  const filteredProducts = products.filter((product) => {
+    return !withoutImageId.includes(product.id);
+  });
+
+  useEffect(() => {
+    const fetchproductList = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(`https://dummyjson.com/products/`);
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchproductList();
+  }, []);
+
+  if (loading) return <Loading />;
+
   return (
     <div className="min-h-screen w-full dark:bg-[#0d1117] dark:text-[#f0f6fc] relative">
-      <div className="max-w-7xl w-full mx-auto"></div>
+      <div className="max-w-7xl w-full mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 p-6">
+          {filteredProducts.map((product) => {
+            return (
+              <Link key={product.id} to={`/product/${product.id}`}>
+                <ProductItem product={product} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };

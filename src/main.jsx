c642363b719191fc322a900511c3acc786ef8881
Jallen Portugal <95646179+Liebe3@users.noreply.github.com>
2025-07-31 18:56:48 +0800

    add AuthProvider and wrapp with app

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+
+//Provider
 import ThemeProvider from "./components/provider/ThemeProvider.jsx";
 import CartProvider from "./components/provider/CartProvider.jsx";
+import AuthProvider from "./components/provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 );
